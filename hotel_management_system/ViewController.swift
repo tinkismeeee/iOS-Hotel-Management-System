56bd83a61d71b5a10d2f15a2d7022a9ab8d775b1
mac <mac@MacBook-Pro.local>
2026-01-26 14:28:18 +0700

    Initial commit

---
+++ hotel_management_system/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotel_management_system
+//
+//  Created by mac on 2026/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
